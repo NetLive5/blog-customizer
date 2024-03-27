02a8639c144c53271e9607557690725db78d85bf
Igor Semenov <igor.nevski.25@gmail.com>
2024-03-27 15:20:54 +0300

    Открытие меню

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,32 @@
+import clsx from 'clsx';
 import arrow from 'src/images/arrow.svg';
-
 import styles from './ArrowButton.module.scss';
-
-/** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type arrowButton = {
+	state: boolean;
+	onOpen: OnClick;
+};
+export const ArrowButton = ({ state, onOpen }: arrowButton) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx({
+				[styles.container]: true,
+				[styles.container_open]: state,
+			})}
+			onClick={onOpen}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx({
+					[styles.arrow]: true,
+					[styles.arrow_open]: state,
+				})}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,24 @@
+import clsx from 'clsx';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-
+import { useState } from 'react';
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [open, setOpen] = useState(false);
+
+	const isOpen = () => {
+		setOpen((prevState) => !prevState);
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton state={open} onOpen={isOpen} />
+			<aside
+				className={clsx({
+					[styles.container]: true,
+					[styles.container_open]: open,
+				})}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
